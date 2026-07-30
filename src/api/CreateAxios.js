63cc5672687ec5f0eb337e62fd11564edd0e32bc
Varun <varungupta00v7@gmail.com>
2026-07-30 17:46:46 +0530

    commit changes

--- src/api/CreateAxios.js
+++ src/api/CreateAxios.js
@@ -1,5 +1,10 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "https://localhost:7030/api"
+    // baseURL: "https://localhost:7030/api"
+    baseURL: "https://nutrition-planner.somee.com/api",
+    timeout: 60000,
+    headers: {
+        "Content-Type": "application/json"
+    }
 });
